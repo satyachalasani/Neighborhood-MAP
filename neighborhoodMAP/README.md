583ae49968045bf07298326133db1256e44b6550
satyachalasani <satyachalasani7@gmail.com>
2018-05-16 08:39:02 -0700

    Add files via upload

---
+++ neighborhoodMAP/README.md
@@ -0,0 +1,15 @@
+#Neighborhood Map Project
+
+
+Created as a project for Udacity's Full-stack Nanodegree program 
+
+##Setup:
+download the file.
+
+Extract files.
+
+Open index.html
+
+##Screenshot
+![](/Users/sreesatya/Documents/neighborhoodMAP/neighbourhood-map.png)
+
---
+++ neighborhoodMAP/js/data.js
@@ -0,0 +1,47 @@
+/* Model Data */
+
+var locations = [
+    {
+        title: 'Winchester Mystery House',
+        location: {
+            lat: 37.3183,
+            lng: -121.9510
+        }
+    },
+    {
+        title: 'Mission Peak',
+        location: {
+            lat: 37.5124,
+            lng: -121.8805
+        }
+    },
+    {
+        title: 'The Tech Museum of Innovation',
+        location: {
+            lat: 37.3316,
+            lng: -121.8901
+        }
+    },
+    {
+        title: 'Alum Rock Park',
+        location: {
+            lat: 37.3969,
+            lng: -121.8023
+        }
+    },
+    {
+        title: 'Sunnyvale Downtown',
+        location: {
+            lat: 37.376824,
+            lng: -122.03435
+        }
+    },
+	{
+        title: 'Main Street Cupertino',
+        location: {
+            lat: 37.3232,
+            lng: -122.0105
+        }
+    }
+    
+];
